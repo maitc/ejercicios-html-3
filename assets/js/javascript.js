681773c99886c0fe600781b4a6b8552fdd17ed1e
maitc <maibeet.torres@gmail.com>
2017-04-24 22:07:41 -0400

    listo1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,6 @@
 function convertirF(){
 	var cel = parseFloat(document.getElementById('celsius').value);//recibe el numero ingresado
-	var res = ((cel * 9 / 5) + 32).toFixed(2);//realiza calculo
+	var res = ((cel * 9 / 5) + 32).toFixed(2) + "°F";//realiza calculo
 	document.getElementById('fa').value=res;//se imprime resultado en cuadrado.
 
 };
